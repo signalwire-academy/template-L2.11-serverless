7c93fd0537383c3243705f9a9366394383cbd7ac
Brian West <brian@signalwire.com>
2025-12-07 05:48:58 -0800

    Add starter template

---
+++ reference/starter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Lab 2.11: Serverless Deployment - Starter Template"""
+
+from signalwire_agents import AgentBase, SwaigFunctionResult
+
+# TODO: Implement your agent
+
+if __name__ == "__main__":
+    pass
